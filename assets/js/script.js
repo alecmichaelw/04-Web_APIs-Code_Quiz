c9e7724690ed827c783ffc43c1d0279026498208
Alec Westbrook <alecmichaelw@gmail.com>
2022-06-08 18:02:14 -0400

    added sudo, created baseline html and css

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+/*
+step one: display start page - title, paragraph, start button
+
+step two: create timer that will start when button is clicked, hide start page, display quesiton, four answer buttons. When selection is made, comapred to correct or wrong. Start clock at 75, wrong answer subtracts by 15 and shows wrong message; correct answer no penalty and shows correct message.
+
+step three: Once finished with all quesitons, score page will be presented with an input of your initials and submit button. The timer should stop and time left becomes your score. when you click submit, should store in localstorage
+
+step four: show dashboard of all scores.
+
+*/
