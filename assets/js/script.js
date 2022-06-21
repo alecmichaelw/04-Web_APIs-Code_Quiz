0f8b5dda50a0fc3744b0f3c97c9db25651d19460
Alec Westbrook <alecmichaelw@gmail.com>
2022-06-21 10:24:59 -0400

    fixing timer, adding questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,100 @@
-var startButton = document.getElementById("start-btn")
-var questionContainerEl = document.getElementById("question-container")
+const startButton = document.getElementById("start-btn")
+const questionContainerEl = document.getElementById("question-container")
+const questionEl = document.getElementById("question")
+const answerBtnEl = document.getElementById("answer-buttons")
+var timerEl = document.getElementById('time')
+var timeRemaining = 75 // for 5 questions
+var clockid
+let shuffledQuestions, currentQuestionIndex
+
+const questions = [
+    {
+        question: "what is 2 + 2?",
+        answers: [
+            {text: "4", correct: true},
+            {text: "33", correct: false},
+            {text: "44", correct: false},
+            {text: "77", correct: false}
+        ]
+    },
+
+    {
+        question: "what is 1 + 1?",
+        answers: [
+            {text: "4", correct: false},
+            {text: "2", correct: true}
+        ]
+    }
+]
+
+function countDown() {
+    timerEl.textContent = timeRemaining
+    timeRemaining--
+
+}
 
 function startGame() {
     console.log("started")
     startButton.classList.add("hide")
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
     questionContainerEl.classList.remove("hide")
+    clockid = setInterval(countDown, 1000)
+    setNextQuestion()
 }
-startButton.addEventListener("click", startGame)
-// function selectAnswer {
 
-// }
+
+function setNextQuestion() {
+    resetState()
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question) {
+    questionEl.innerText = question.question
+    question.answers.forEach(answer => {
+        const button = document.createElement("button")
+        button.innerText = answer.text
+        button.classList.add("btn")
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener("click", selectAnswer)
+        answerBtnEl.appendChild(button)
+    })
+}
+
+
+function selectAnswer(e) {
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerBtnEl.children).forEach(button => {
+        setStatusClass(button, button. dataset.correct)
+    })
+}
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add("correct")
+    } else {
+        element.classList.add("wrong")
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove("correct")
+    element.classList.remove("wrong")
+}
+
+function resetState() {
+    while (answerBtnEl.firstChild) {
+      answerBtnEl.removeChild(answerBtnEl.firstChild)  
+    }
+}
+
+
+startButton.addEventListener("click", startGame)
 
 
 
@@ -24,15 +109,7 @@ step four: show dashboard of all scores.
 
 */
 
-// var timeRemaining = 75 // for 5 questions
 
-// function countDown() {
-//     timerEl.textContent = timeRemaining
-//     timeRemaining--
 
-// }
 
-// function startGame() {
-
-// }
 
